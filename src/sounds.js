7846acf06ac8b4ce2979c199c377c5ecb214a84d
DaviCarvalho5 <davicarvalhocontato@gmail.com>
2021-06-13 19:29:43 -0300

    Trying to solve the sound problem 1

--- src/sounds.js
+++ src/sounds.js
@@ -1,13 +1,13 @@
 export default class Sounds {
   constructor () {
-    this.theme = this.addAudio('../assets/sounds/theme.wav', true);
-    this.hit = this.addAudio('../assets/sounds/hit.wav', false);
-    this.break = this.addAudio('../assets/sounds/break.wav', false);
-    this.newBall = this.addAudio('../assets/sounds/new-ball.wav', false);
-    this.gameOver = this.addAudio('../assets/sounds/game-over.wav', false);
-    this.end = this.addAudio('../assets/sounds/end.wav', false);
-    this.nextLevel = this.addAudio('../assets/sounds/next-level.wav', false);
-  }
+    this.theme = this.addAudio('https://raw.githubusercontent.com/DaviCarvalho5/cool-paddle-game-2000/master/assets/sounds/theme.wav', true);
+    this.hit = this.addAudio('https://raw.githubusercontent.com/DaviCarvalho5/cool-paddle-game-2000/master/assets/sounds/hit.wav', false);
+    this.break = this.addAudio('https://raw.githubusercontent.com/DaviCarvalho5/cool-paddle-game-2000/master/assets/sounds/break.wav', false);
+    this.newBall = this.addAudio('https://raw.githubusercontent.com/DaviCarvalho5/cool-paddle-game-2000/master/assets/sounds/new-ball.wav', false);
+    this.gameOver = this.addAudio('https://raw.githubusercontent.com/DaviCarvalho5/cool-paddle-game-2000/master/assets/sounds/game-over.wav', false);
+    this.end = this.addAudio('https://raw.githubusercontent.com/DaviCarvalho5/cool-paddle-game-2000/master/assets/sounds/end.wav', false);
+    this.nextLevel = this.addAudio('https://raw.githubusercontent.com/DaviCarvalho5/cool-paddle-game-2000/master/assets/sounds/next-level.wav', false);
+   }
 
   play(sound) {
     sound.play();
